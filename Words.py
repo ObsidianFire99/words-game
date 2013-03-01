27baa1a84979d5861cdc7e5ccc1f0fa28e2466b2
ObsidianFire99 <Obsidianfire99@gmail.com>
2013-03-01 21:20:31 +0000

    Create Words.py
    The 0.1.0 Game Code.

---
+++ Words.py
@@ -0,0 +1,48 @@
+def startmessage():
+    print name
+    return None
+def chamberone():
+    # Placeholder
+    return None
+def chambertwo():
+    # Placeholder
+    return None
+def chamberthree():
+    # Placeholder
+    return None
+def chamberfour():
+    # Placeholder
+    return None
+def chamberfive():
+    # Placeholder
+    return None
+def chambersix():
+    # Placeholder
+    return None
+def chamberseven():
+    # Placeholder
+    return None
+def chambereight():
+    # Placeholder
+    return None
+def chambernine():
+    # Placeholder
+    return None
+    
+def start():
+    inlogo = open('logo.txt', 'r')
+    for i in inlogo:
+        print i
+    print ""
+    print ""
+    print "A OneTen Production"
+    def choice():
+        userchoice = raw_input("Type P for Play!!!")
+        if choice == "P":
+            username = raw_input("How do they call you in the lands you are from: "
+            ()
+        else:
+            choice()
+    choice()
+        
+start()
