3a5ec4bf0d6d103d9d46d0a9062e40a125f631c5
Obsidian Fire <ObsidianFire99@gmail.com>
2013-03-02 11:19:11 +0000

    Update Batch with Help text

--- Words.bat
+++ Words.bat
@@ -1,2 +1,3 @@
 python Words.py
-ECHO -+-+-+-+-+
+# If this Batch File dosn't work, you may need to install python 2.7.3
+# You may also need to configure your PATH Environment Variable to point to your main python directory (Normallly C:\Python27)
